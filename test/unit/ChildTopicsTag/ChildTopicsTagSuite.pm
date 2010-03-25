40a74615f839f1e4104136e87038e3b4c2bc4c24
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2010-03-25 14:51:12 +0000

    Item8765: Replace use base with use; our @ISA;
    git-svn-id: http://svn.foswiki.org/trunk/ChildTopicsTag@6912 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- test/unit/ChildTopicsTag/ChildTopicsTagSuite.pm
+++ test/unit/ChildTopicsTag/ChildTopicsTagSuite.pm
@@ -1,6 +1,7 @@
 package ChildTopicsTagSuite;
 
-use base qw(Unit::TestSuite);
+use Unit::TestSuite;
+our @ISA = qw( Unit::TestSuite );
 
 sub name { 'ChildTopicsTagSuite' };
 
--- test/unit/ChildTopicsTag/ChildTopicsTagTests.pm
+++ test/unit/ChildTopicsTag/ChildTopicsTagTests.pm
@@ -2,7 +2,8 @@ use strict;
 
 package ChildTopicsTagTests;
 
-use base qw(FoswikiTestCase);
+use FoswikiTestCase;
+our @ISA = qw( FoswikiTestCase );
 
 use strict;
 use TWiki;
